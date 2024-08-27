dd90e6cfcf15854cb3d58216e217675f25034101
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-08-27 23:13:13 +0200

    chore: Refactor data types and customize questions

--- frontend-kiosco-angular/src/app/components/confirm-modal/confirm-modal.component.ts
+++ frontend-kiosco-angular/src/app/components/confirm-modal/confirm-modal.component.ts
@@ -1,25 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Menu, Product } from '../../interfaces/pedido';
+import { CustomizationOption, CustomizationQuestion, Menu, Product } from '../../interfaces/pedido';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-confirm-modal',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './confirm-modal.component.html',
-  styleUrl: './confirm-modal.component.css'
+  styleUrl: './confirm-modal.component.css',
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('300ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class ConfirmModalComponent {
-  @Input() data: any;
+  // @Input() product!: Product | Menu;
+  //   export interface Product {
+  //     id: string;
+  //     name: string;
+  //     img?: string;
+  //     price: number;
+  //     description: string;
+  //     familyId: string;
+  //     customizations?: CustomizationResponse[];  // Opcional, puede no tener personalizaciones
+  //     customizationQuestions?: CustomizationQuestion[];  // Opcional, puede no tener preguntas de personalización
+  // }
+  // Tener un producto con datos de prueba:
+  // Ejemplo de datos de prueba para un producto
+  product!: Product | Menu
+  
+
   @Output() confirmAction = new EventEmitter<number>();
   @Output() cancelAction = new EventEmitter<void>();
   quantity: number = 1;
 
+  currentCustomizationIndex: number = 0;
+
   isVisible = false;
 
   open(data: any): void {
-    this.data = data;
+    this.product = data;
     this.isVisible = true;
     setTimeout(() => {
       const modalContainer = document.querySelector('.modal-container') as HTMLElement;
@@ -29,6 +59,30 @@ export class ConfirmModalComponent {
         modalOverlay.classList.add('show');
       }
     }, 0);
+  }
+
+  thereAreQuestionsLeft(): boolean {
+    if (this.product.customizationQuestions && this.product.customizationQuestions.length > 0) {
+      console.log(this.currentCustomizationIndex < this.product.customizationQuestions.length);
+      return this.currentCustomizationIndex < this.product.customizationQuestions.length;
+    }
+    console.log('No hay preguntas de personalización');
+    return false;
+  }
+
+  selectOption(option : CustomizationOption): void {
+    if(this.product.customizations) {
+      this.product.customizations[this.currentCustomizationIndex] = {name: this.product.customizationQuestions?.[this.currentCustomizationIndex]?.name, value: option.value, price: option.price};
+    }
+    console.log(this.product.customizations);
+  }
+
+  previousCustomization(): void {
+    this.currentCustomizationIndex--;
+  }
+
+  nextCustomization(): void {
+    this.currentCustomizationIndex++;
   }
 
   increaseQuantity(): void {
@@ -42,6 +96,7 @@ export class ConfirmModalComponent {
   }
 
   close(): void {
+    console.log('close');
     const modalContainer = document.querySelector('.modal-container') as HTMLElement;
     const modalOverlay = document.querySelector('.modal-overlay') as HTMLElement;
     if (modalContainer && modalOverlay) {
@@ -58,4 +113,5 @@ export class ConfirmModalComponent {
     this.confirmAction.emit(this.quantity);
     this.close();
   }
+  
 }
