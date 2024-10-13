efff123938af24be0cca83c7f8c041625dd97652
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-10-14 00:22:31 +0200

    Refactor styles and add table component for management panel

--- frontend-kiosco-angular/src/app/pages/layouts/management-panel/overview/overview.component.ts
+++ frontend-kiosco-angular/src/app/pages/layouts/management-panel/overview/overview.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OverviewGraphComponent } from './overview-graph/overview-graph.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [OverviewGraphComponent],
+  imports: [OverviewGraphComponent, TableComponent],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
